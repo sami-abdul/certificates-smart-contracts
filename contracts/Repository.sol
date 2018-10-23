29f9204b5d5e8f3d42d05a2f345c14497ee86f4f
Taha haq <haqtaha@gmail.com>
2018-10-23 10:18:32 +0500

    refactored spelling

--- contracts/Repository.sol
+++ contracts/Repository.sol
@@ -6,7 +6,7 @@ import "./AccessControl.sol";
 contract Repository is Ownable {
     
     struct Certificate {
-        bytes32 certficateId;
+        bytes32 certificateId;
         bytes32 studentId;
         bytes32 courseId;
         bytes32 studentName;
@@ -36,7 +36,7 @@ contract Repository is Ownable {
     function getCertificatesByStudentId(bytes32 _stdId) public view returns(bytes32[], bytes32[], bytes32[], bytes32[], uint256[]) {
         bytes32[] certIds = stdIdToCertificateIds[_stdId];
         
-        bytes32[] memory certficateIds_ = new bytes32[](certIds.length);
+        bytes32[] memory certificateIds_ = new bytes32[](certIds.length);
         bytes32[] memory courseIds = new bytes32[](certIds.length);
         bytes32[] memory studentNames = new bytes32[](certIds.length);
         bytes32[] memory courseNames = new bytes32[](certIds.length);
@@ -44,18 +44,18 @@ contract Repository is Ownable {
 
         for (uint i = 0; i < certIds.length; i++) {
             Certificate memory cert = idToCertificate[certIds[i]];
-            certficateIds_[i] = cert.certficateId;
+            certificateIds_[i] = cert.certificateId;
             courseIds[i] = cert.courseId;
             studentNames[i] = cert.studentName;
             courseNames[i] = cert.courseName;
             issuedDates[i] = cert.issuedDate;
         }
 
-        return (certficateIds_, courseIds, studentNames, courseNames, issuedDates);
+        return (certificateIds_, courseIds, studentNames, courseNames, issuedDates);
     }
 
     function getAllCertificates() public view returns(bytes32[], bytes32[], bytes32[], bytes32[], bytes32[], uint256[], bool[]) {
-        bytes32[] memory certficateIds_ = new bytes32[](certificateIds.length);
+        bytes32[] memory certificateIds_ = new bytes32[](certificateIds.length);
         bytes32[] memory studentIds = new bytes32[](certificateIds.length);
         bytes32[] memory courseIds = new bytes32[](certificateIds.length);
         bytes32[] memory studentNames = new bytes32[](certificateIds.length);
@@ -65,7 +65,7 @@ contract Repository is Ownable {
 
         for (uint i = 0; i < certificateIds.length; i++) {
             Certificate memory cert = idToCertificate[certificateIds[i]];
-            certficateIds_[i] = cert.certficateId;
+            certificateIds_[i] = cert.certificateId;
             studentIds[i] = cert.studentId;
             courseIds[i] = cert.courseId;
             studentNames[i] = cert.studentName;
@@ -74,7 +74,7 @@ contract Repository is Ownable {
             revokeStatuses[i] = cert.isRevoked;
         }
 
-        return (certficateIds_, studentIds, courseIds, studentNames, courseNames, issuedDates, revokeStatuses);
+        return (certificateIds_, studentIds, courseIds, studentNames, courseNames, issuedDates, revokeStatuses);
     }
 
     function addCertificate(bytes32 _stdId, bytes32 _crsId, bytes32 _stdName, bytes32 _crsName, uint256 _date) public onlyOwner returns(bytes32) {
