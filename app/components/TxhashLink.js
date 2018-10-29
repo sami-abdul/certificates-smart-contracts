eabc4e5c25a30d454f23554b72023f7bb84f36f5
Abdul Sami <sami-abdul@outlook.com>
2018-10-29 11:07:54 +0500

    Move web app to another repo

--- app/components/TxhashLink.js
+++
@@ -1,9 +0,0 @@
-var Results = React.createClass({
-    render: function() {
-        return (
-            <div id="results" className="search-results">
-                Some Results
-            </div>
-        );
-    }
-});
--- routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('next-routes')();
-
-routes
-    .add('index', 'index')
-    .add('/certificate/new', '/certificate/new')
-    .add('/certificate/:address', '/certificate/detail');
-
-
-module.exports = routes;
